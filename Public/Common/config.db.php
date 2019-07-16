a5a3fae2d8abb9c03883e8c7ed2a7c422d8291d4
winter2012 <winter2012102@gmail.com>
2019-07-16 19:35:28 +0800

    first commit

---
+++ Public/Common/config.db.php
@@ -0,0 +1,13 @@
+<?php
+ return array (
+  'DB_HOST' => 'localhost',
+  'DB_PORT' => '3306',
+  'DB_USER' => 'lingmengdh_com',
+  'DB_NAME' => 'lingmengdh_com',
+  'DB_PWD' => 'F2try3ApXBMsGDyA',
+  'DB_PREFIX' => 'cc_',
+  'ROOT_FILE' => '/',
+  'DEFAULT_THEME' => 'Black',
+  'URL_MODEL' => '0',
+)
+?>
